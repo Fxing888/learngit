f16ab24da7ea59018fecacfa829c1beaeb658261
F <3602723170@qq.com>
2025-11-22 12:15:02 +0800

    change num1

---
+++ TestC++/Test1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "This is Test1.cpp" << endl;
+    return 0;
+}
---
+++ TestC++/Test2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Just test!" << endl;
+    cout << "This is Test2.cpp" << endl;
+    return 0;
+}
