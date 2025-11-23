7229692bd2a39da4a1c0b985b3fc7e6bad83f723
F <3602723170@qq.com>
2025-11-24 00:42:55 +0800

    branch test

--- TestC++/Test1.cpp
+++ TestC++/Test1.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "This is Test1.cpp" << endl;
     cout << "Just a simple test file." << endl;
+    cout << "Goodbye!" << endl;
     return 0;
 }
